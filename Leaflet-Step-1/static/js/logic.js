951aa82ef69c5962eaf87349a052368bcd39c97f
Ksheekey <76223988+Ksheekey@users.noreply.github.com>
2021-05-26 16:01:01 -0400

    progress

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -17,20 +17,45 @@ var eQuake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_
 
 d3.json(eQuake_url).then(function(response) {
 
-    console.log(response.features[1000]);
+    console.log(response.features[0]);
 
-    for (var i = 0; i < 50; i++) {
+    for (var i = 0; i < 150; i++) {
         var location = response.features[i];
+
+        // Conditionals for countries points
+        var color = "";
+        if (location.geometry.coordinates[2] >= -10 && location.geometry.coordinates[2] < 10) {
+            color = "#99FF00";
+        }
+        else if (location.geometry.coordinates[2] >= 10 && location.geometry.coordinates[2] < 30) {
+            color = "#FFFF33";
+        }
+        else if (location.geometry.coordinates[2] >= 30 && location.geometry.coordinates[2] < 50) {
+            color = "#FFCC00";
+        }
+        else if (location.geometry.coordinates[2] >= 50 && location.geometry.coordinates[2] < 70) {
+            color = "#FF9900";
+        }
+        else if (location.geometry.coordinates[2] >= 70 && location.geometry.coordinates[2] < 90) {
+            color = "#FF6600";
+        }
+        else if (location.geometry.coordinates[2] >= 90) {
+            color = "#FF0000";
+        }
+        else {
+            color = "black";
+        }
 
         if (location) {
         //L.marker([location.geometry.coordinates[1], location.geometry.coordinates[0]]).addTo(myMap);
         L.circle([location.geometry.coordinates[1], location.geometry.coordinates[0]], {
-            fillOpacity: 0.75,
-            color: "orange",
-            fillColor: "orange",
+            fillOpacity: 0.9,
+            color: "black",
+            weight: 1,
+            fillColor: color,
             // Adjust radius
-            radius: location.properties.mag *50000
-        }).bindPopup("<h1>" + "yea" + "</h1> <hr> <h3>Points: " + "yea" + "</h3>").addTo(myMap);
+            radius: location.properties.mag *15000
+        }).bindPopup(`<h3> Type: ${location.properties.type} <hr> Place: ${location.properties.place}<br> Magnitude: ${location.properties.mag}<br> Depth: ${location.geometry.coordinates[2]}</h3>`).addTo(myMap);
         }
     }
 
